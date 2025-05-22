c3df3a0f0cdc1ed7071d1f29fca100235959ec0a
Joseth Alarcon <joseth.30.17@gmail.com>
2025-05-21 23:41:35 -0400

    Primer commit limpio

---
+++ Portafolio/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,54 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Servicios
+{
+    public interface IRepositorioProyectos
+    {
+        List<ProyectoDTO> ObtenerProyectos();
+    }
+    public class RepositorioProyectos: IRepositorioProyectos
+    {
+        public List<ProyectoDTO> ObtenerProyectos()
+        {
+            return new List<ProyectoDTO>() {
+                new ProyectoDTO
+            {
+                Titulo = "BalanceFin Pro",
+                Descripcion = "Proyecto realizado con React y TypeScript",
+                Link = "https://balance-fin-pro-team-joseth.netlify.app/",
+                ImagenURL = "/imagenes/BalanceFin-Pro.jpg"
+            },
+
+                new ProyectoDTO
+            {
+                Titulo = "Gif Total",
+                Descripcion = "Proyecto realizado con React y JavaScript",
+                Link = "https://gif-experto-joseth.netlify.app/",
+                ImagenURL = "/imagenes/Gif-Total.jpg"
+            },
+                new ProyectoDTO
+            {
+                Titulo = "Balance",
+                Descripcion = "Proyecto realizado con React y TypeScript",
+                Link = "https://balance-fin-pro-team-joseth.netlify.app/",
+                ImagenURL = "/imagenes/BalanceFin-Pro.jpg"
+            },
+                new ProyectoDTO
+            {
+                Titulo = "Balance",
+                Descripcion = "Proyecto realizado con React y TypeScript",
+                Link = "https://balance-fin-pro-team-joseth.netlify.app/",
+                ImagenURL = "/imagenes/BalanceFin-Pro.jpg"
+            },
+                new ProyectoDTO
+            {
+                Titulo = "Balance",
+                Descripcion = "Proyecto realizado con React y TypeScript",
+                Link = "https://balance-fin-pro-team-joseth.netlify.app/",
+                ImagenURL = "/imagenes/BalanceFin-Pro.jpg"
+            },
+            };
+
+        }
+    }
+}
